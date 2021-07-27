1a9dc6aef3590cc7c99a39e6ba3e79b2f131a3e6
Maxwell cudlitz <maxwell.cudlitz@gmail.com>
2021-07-27 14:59:30 -0700

    remove venv

--- .github/scripts/configure_env.sh
+++ .github/scripts/configure_env.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-python3 -m venv virtual-env
-source virtual-env/bin/activate
-
-# pip install handled through manual installation rather than requiements.txt
-# due to local files
-
 pip install ./
 pip install pytest-asyncio
